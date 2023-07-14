d21d99a2145286fe03f6f4318c319cc24479850d
sandovaldavid <sandovaldavid2201@gmai.com>
2023-07-14 16:59:48 -0500

    Interfaz CompactacionInSitu para la calse cine creada

--- src/Cine/Mantenimiento/CompactarInSitu.java
+++ src/Cine/Mantenimiento/CompactarInSitu.java
@@ -14,14 +14,14 @@ import java.util.logging.Logger;
  *
  * @author David Sandoval Salvador
  */
-public class CompactarPorCopia extends javax.swing.JInternalFrame {
+public class CompactarInSitu extends javax.swing.JInternalFrame {
 
     Cine cine = getCine();
 
     /**
      * Creates new form CompactarPorCopia
      */
-    public CompactarPorCopia() {
+    public CompactarInSitu() {
         initComponents();
     }
 
@@ -34,17 +34,17 @@ public class CompactarPorCopia extends javax.swing.JInternalFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        btnCompactarCopia = new javax.swing.JButton();
+        btnCompactarInsitu = new javax.swing.JButton();
 
         setClosable(true);
 
-        btnCompactarCopia.setBackground(new java.awt.Color(0, 153, 51));
-        btnCompactarCopia.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
-        btnCompactarCopia.setForeground(new java.awt.Color(255, 255, 255));
-        btnCompactarCopia.setText("Compactar");
-        btnCompactarCopia.addActionListener(new java.awt.event.ActionListener() {
+        btnCompactarInsitu.setBackground(new java.awt.Color(0, 153, 51));
+        btnCompactarInsitu.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        btnCompactarInsitu.setForeground(new java.awt.Color(255, 255, 255));
+        btnCompactarInsitu.setText("Compactar");
+        btnCompactarInsitu.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                btnCompactarCopiaActionPerformed(evt);
+                btnCompactarInsituActionPerformed(evt);
             }
         });
 
@@ -54,30 +54,30 @@ public class CompactarPorCopia extends javax.swing.JInternalFrame {
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addGap(141, 141, 141)
-                .addComponent(btnCompactarCopia)
+                .addComponent(btnCompactarInsitu)
                 .addContainerGap(169, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addGap(41, 41, 41)
-                .addComponent(btnCompactarCopia)
+                .addComponent(btnCompactarInsitu)
                 .addContainerGap(51, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void btnCompactarCopiaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnCompactarCopiaActionPerformed
+    private void btnCompactarInsituActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnCompactarInsituActionPerformed
         try {
-            cine.CompactacionCopia();
+            cine.CompactacionInSitu();
         } catch (IOException ex) {
-            Logger.getLogger(CompactarPorCopia.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(CompactarInSitu.class.getName()).log(Level.SEVERE, null, ex);
         }
-    }//GEN-LAST:event_btnCompactarCopiaActionPerformed
+    }//GEN-LAST:event_btnCompactarInsituActionPerformed
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton btnCompactarCopia;
+    private javax.swing.JButton btnCompactarInsitu;
     // End of variables declaration//GEN-END:variables
 }
