220be2d15d4adaffff47b2479f60033557561798
sandovaldavid <sandovaldavid2201@gmai.com>
2023-07-21 12:34:42 -0500

    feat: Se agrego la funcionalidad de listado de indices --> P y S

--- src/Cine/Indice/ListarIndice.java
+++ src/Cine/Indice/ListarIndice.java
@@ -12,14 +12,14 @@ import static Main.MenuPrincipal.getCine;
  *
  * @author David Sandoval Salvador
  */
-public class ListarIndicePrimario extends javax.swing.JInternalFrame {
+public class ListarIndice extends javax.swing.JInternalFrame {
 
     Cine cine = getCine();
 
     /**
      * Creates new form ListarIndicePrimario
      */
-    public ListarIndicePrimario() {
+    public ListarIndice() {
         initComponents();
     }
 
@@ -32,14 +32,24 @@ public class ListarIndicePrimario extends javax.swing.JInternalFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        btnListarIndicePrimario = new javax.swing.JButton();
+        btnListarIndiceP = new javax.swing.JButton();
+        btnListarIndiceS = new javax.swing.JButton();
 
         setClosable(true);
+        setTitle("Listado Indice");
+        setToolTipText("");
 
-        btnListarIndicePrimario.setText("Listar Indice");
-        btnListarIndicePrimario.addActionListener(new java.awt.event.ActionListener() {
+        btnListarIndiceP.setText("Listar Indice P");
+        btnListarIndiceP.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                btnListarIndicePrimarioActionPerformed(evt);
+                btnListarIndicePActionPerformed(evt);
+            }
+        });
+
+        btnListarIndiceS.setText("Listar Indice S");
+        btnListarIndiceS.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnListarIndiceSActionPerformed(evt);
             }
         });
 
@@ -49,31 +59,45 @@ public class ListarIndicePrimario extends javax.swing.JInternalFrame {
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addGap(99, 99, 99)
-                .addComponent(btnListarIndicePrimario)
-                .addContainerGap(111, Short.MAX_VALUE))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(btnListarIndiceS)
+                    .addComponent(btnListarIndiceP))
+                .addContainerGap(101, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addGap(24, 24, 24)
-                .addComponent(btnListarIndicePrimario)
-                .addContainerGap(32, Short.MAX_VALUE))
+                .addComponent(btnListarIndiceP)
+                .addGap(18, 18, 18)
+                .addComponent(btnListarIndiceS)
+                .addContainerGap(37, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void btnListarIndicePrimarioActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnListarIndicePrimarioActionPerformed
+    private void btnListarIndicePActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnListarIndicePActionPerformed
         System.out.println("Listado del Indice Primario en Memoria");
         int i = 0;
         for (Nodo n : cine.getIndicePrimario()) {
             System.out.println("pos " + i + ": " + n.toString());
             i++;
         }
-    }//GEN-LAST:event_btnListarIndicePrimarioActionPerformed
+    }//GEN-LAST:event_btnListarIndicePActionPerformed
+
+    private void btnListarIndiceSActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnListarIndiceSActionPerformed
+        System.out.println("Listado del Indice Secundario en Memoria");
+        int i = 0;
+        for (Nodo n : cine.getIndiceSecundario()) {
+            System.out.println("pos " + i + ": " + n.toString());
+            i++;
+        }
+    }//GEN-LAST:event_btnListarIndiceSActionPerformed
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton btnListarIndicePrimario;
+    private javax.swing.JButton btnListarIndiceP;
+    private javax.swing.JButton btnListarIndiceS;
     // End of variables declaration//GEN-END:variables
 }
